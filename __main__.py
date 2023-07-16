3cfc94891980f10676c6c55e36290fec4c258ac6
Ghufran <ghufranshahid101@gmail.com>
2023-07-16 02:05:30 -0400

    Added boilerplate for discord bot

--- __main__.py
+++ __main__.py
@@ -1,1 +1,20 @@
-import config
+import config, discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print('Ready')
+
+@client.event
+async def on_message(message):
+
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hello!")
+        
+
+
+client.run(config.API_KEY)
