4ab90b03fb296cc098e6f433e98bdbdddd84cc3d
Ghufran <ghufranshahid101@gmail.com>
2023-07-17 17:22:17 -0400

    Added description, alongside colour coding for  command embeds

--- formatMessages.py
+++ formatMessages.py
@@ -3,8 +3,9 @@ import discord
 def createEmbed(data):
     name = data["data"][0]["titles"][0]["title"]
     embedImg = data["data"][0]["images"]["jpg"]["image_url"]
+    description = data["data"][0]["synopsis"]
 
-    newEmbed = discord.Embed(title=name)
+    newEmbed = discord.Embed(title=name, color=discord.Colour.from_rgb(219, 172, 52), description=description)
     newEmbed.set_image(url=embedImg)
 
     return newEmbed
