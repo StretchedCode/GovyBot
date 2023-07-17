34bf78a03eafe1ff571300c5ddb61d14fd26a977
Ghufran <ghufranshahid101@gmail.com>
2023-07-17 17:01:07 -0400

    Added checking for 'anime' command, imported asyncio to manage timeout exceptions; creates an embed image with formatted data once user responds with query

--- __main__.py
+++ __main__.py
@@ -1,4 +1,4 @@
-import config, discord
+import config, discord, api, asyncio, formatMessages
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -13,7 +13,28 @@ async def on_ready():
 async def on_message(message):
 
     if message.content.startswith('$hello'):
+
         await message.channel.send("Hello!")
+
+    elif message.content.startswith('$anime'):
+
+        def id_check(m):
+            return len(m.content) >- 1
+
+        await message.channel.send('Please enter the name of the anime you would like to search for: ')
+        
+        try:
+            msg = await client.wait_for('message', check=id_check, timeout=10.0)
+        except asyncio.TimeoutError:
+            await message.channel.send("Request failed")
+        else:
+            await message.channel.send(f"Request succeeded, {msg.author}")
+
+            data = api.getAnimeData(msg.content)
+            embedMsg = formatMessages.createEmbed(data)
+
+            await message.channel.send(embed=embedMsg)
+        
         
 
 
